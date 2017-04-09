ea8c134e676e37936d1f775c47b305d01aa4bb35
James Hartanto <jameshartanto1993@gmail.com>
2017-04-09 13:12:41 -0400

    might have to delete main class so that we only generate ONE set of values for the game, Main was just to see that it worked

--- src/main/java/com/theironyard/SnakesAndLaddersController.java
+++ src/main/java/com/theironyard/SnakesAndLaddersController.java
@@ -1,11 +1,24 @@
 package com.theironyard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by JamesHartanto on 4/6/17.
  */
 @Controller
 public class SnakesAndLaddersController {
+
+    // Web root mapping
+    @RequestMapping("/")
+    public String showIndex(Model model){
+
+
+        return "index";
+
+    }
+
 
 }
