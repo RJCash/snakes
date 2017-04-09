b7d0a18bb340a23314a64651e89398a262144b82
James Hartanto <jameshartanto1993@gmail.com>
2017-04-09 13:45:22 -0400

    notes on how to tackle problem

--- src/main/java/com/theironyard/SnakesAndLaddersController.java
+++ src/main/java/com/theironyard/SnakesAndLaddersController.java
@@ -8,13 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by JamesHartanto on 4/6/17.
  */
+
+// How to make it work? PLAN A
+    // 1) Create the arraylist of array of ints that resemble the whole game
+    // 2) send the game to javascript, moves
+// Things to be aware of
+    // 1) Do NOT want to reference the oneGame method while the game is playing - do not want to create another set of
+        // array of ints...
+    
 @Controller
 public class SnakesAndLaddersController {
 
+    @Autowired
+    SnakesAndLaddersService service;
+
     // Web root mapping
     @RequestMapping("/")
-    public String showIndex(Model model){
-
+    public String oneGame(Model model){
+        model.addAttribute("oneGame",service.wholeGame());
 
         return "index";
 
